8ac3fe11368994d911b2f1592d7e84c916d22b79
neal <neal.easterling@gmail.com>
2024-04-24 17:27:44 -0500

    refactor to add todo component

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function App(){
     setTodos(newTodos);
   }
   
-  const removeTodo = e =>{
-    const index = Number(e.target.id);
+  const removeTodo = index =>{
     let temp = [...todos];
     temp.splice(index, 1);
     setTodos(temp);
@@ -28,7 +27,7 @@ function App(){
 
   return(
     <>
-      {todos.map((todo, i) => <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
+      {todos.map((todo, i) => <Todo index={i} key={i} todo={todo} remove={removeTodo} />)}
       <TodoForm addTodo={addTodo} />
     </>
   );
