535974af4166d62a1800f831b2332332b5e755d6
neal <neal.easterling@gmail.com>
2024-04-24 17:01:26 -0500

    updated with add and remove

--- index.js
+++ index.js
@@ -14,9 +14,27 @@ function App(){
     }        
   ])
 
+  const [value, setValue] = React.useState('');
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, {text:value, isCompleted:false}];
+    setTodos(newTodos);
+    setValue('');
+  }
+  const removeTodo = e =>{
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
   return(
     <>
-      {todos.map((todo, i) => <div className="todo" key={i}>{todo.text}</div>)}
+      {todos.map((todo, i) => <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
+      <form onSubmit={handleSubmit}>
+        <input type='text' className='input' value={value} placeholder="Add Todo..." onChange={e=> setValue(e.target.value)} />
+      </form>
     </>
   );
 }
